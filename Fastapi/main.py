c68818e87cdbb8e0c78fd783e615f89a3bedf90a
jflabres@gmail.com <74658593+serbal93@users.noreply.github.com>
2025-01-22 17:06:14 -0300

    init project

---
+++ Fastapi/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+items = []
+
+@app.get("/")
+
+def root():
+    return {"Hello": "World"}
+
+
+@app.post("/items")
+
+def create_item(item: str):
+    items.append(item)
+    return items
+
+@app.get("/items")
+def list_items(limit: int = 10):
+    return items[0:limit]
+
+def create_item(item: str):
+    items.append(item)
+    return items
+
+@app.get("/items/{item_id}")
+
+def get_item(item_id: int) -> str:
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code = 404, detail= f'Item {item_id} not found')
+   
